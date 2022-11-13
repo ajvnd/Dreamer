8ea98b274cdcd0cfc82006de25ec868e2d25fcac
Ahmad Javadi <ahmadjavadi17@gmail.com>
2022-11-13 22:40:46 +0330

    add new dbSet.

--- src/Persistence/Dreamer.Persistence/DreamerContext.cs
+++ src/Persistence/Dreamer.Persistence/DreamerContext.cs
@@ -1,8 +1,9 @@
+using Dreamer.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dreamer.Persistence;
 
 public class DreamerContext : DbContext
 {
-    
+    public Sample Sample { get; set; }
 }
