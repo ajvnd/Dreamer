8f822251e8b747651511720812212e58c6ecb35f
Ahmad Javadi <ahmadjavadi17@gmail.com>
2022-11-13 23:02:13 +0330

    fix name inconsistency.

--- src/Application/Dreamer.Application/Profiles/SampleProfile.cs
+++ src/Application/Dreamer.Application/Profiles/SampleProfile.cs
@@ -2,7 +2,7 @@ using AutoMapper;
 using Dreamer.Application.Models.SampleModel;
 using Dreamer.Domain.Entity;
 
-namespace Dreamer.Application.Mappers;
+namespace Dreamer.Application.Profiles;
 
 public class SampleProfile : Profile
 {
