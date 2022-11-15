87711d382f67ca51b69d1e4e9bcee812ef192b63
Ahmad Javadi <ahmadjavadi17@gmail.com>
2022-11-15 19:02:34 +0330

    initial commit.

---
+++ src/Infrastructure/Dreamer.Infrastructure/ConfigureService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dreamer.Infrastructure;
+
+public static class ConfigureService
+{
+    public static IServiceCollection Config(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        return services;
+    }
+}
